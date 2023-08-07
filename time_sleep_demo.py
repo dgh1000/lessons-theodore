1aec78268bc8ab527d46cb0615ff87f911e35f4f
Theodore <treeodoreg@gmail.com>
2023-08-06 17:03:42 -0700

    Change Limits
    Summary

--- time_sleep_demo.py
+++ time_sleep_demo.py
@@ -3,11 +3,11 @@ import time
 x = 0
 dir = 1
 while True:
-    if x >= 5:
+    if x >= 10:
         dir = -1
     if x <= 0:
         dir = 1
     x += dir
     print(x)
-    time.sleep(1)
+    time.sleep(0.5)
 
