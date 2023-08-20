23b4191cdc39d9ff0f014278c1752bbf9cb1fdb9
Michael Mossey <michaelmossey@gmail.com>
2023-08-20 14:38:32 -0700

    homework

---
+++ homework/codewars/8kyu_sum_of_positive.py
@@ -0,0 +1,15 @@
+# Codewars kata: 8kyu Sum of positive
+# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+
+# Extra homework:
+
+# 1. given an array of number, return the sum of every other number
+#    def sum_every_other(numbers):
+
+# 2. given an array of numbers, return the sum of the first and last number
+#    def sum_first_and_last(numbers):
+
+# 3. given an array of numbers, return the sum of every third number
+#    def sum_every_third(numbers):
+
+
---
+++ homework/oo/oo1.py
@@ -0,0 +1,48 @@
+# Here's an example of a class that represents a person
+# with a name and age.  The class has a method that returns
+# the person's name and age as a string.
+#
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def get_name_and_age(self):
+#         return f"{self.name} is {self.age} years old."
+#
+# # Create a Person object and print the result of calling
+# # its get_name_and_age method.
+# person = Person("Joe", 34)
+# print(person.get_name_and_age())
+#
+
+# 1. Write a class called "Car" that has the following properties:
+#    - a "color" property that is set in the constructor
+#    - a "speed" property that is set in the constructor
+#    - a "description" method that returns "{color} car going {speed} mph"
+#    - a "accelerate" method that increases the speed by 1
+#    - a "brake" method that decreases the speed by 1
+#    - a "stop" method that sets the speed to 0
+
+# Here's an example of using the class Car:
+# car = Car("red", 50)
+# print(car.description())
+# car.accelerate()
+# print(car.description())
+# car.brake()
+# print(car.description())
+# car.stop()
+# print(car.description())
+
+# 2. Write a class called "Rectangle" that has the following properties:
+#    - a "width" property that is set in the constructor
+#    - a "height" property that is set in the constructor
+#    - a "area" method that returns the area of the rectangle
+#    - a "perimeter" method that returns the perimeter of the rectangle
+#
+# Here's an example of using the class Rectangle:
+# rectangle = Rectangle(10, 20)
+# print(rectangle.area())
+# print(rectangle.perimeter())
+
+
