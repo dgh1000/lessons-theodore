54faf0551a4f9e258c0727aa6d907026cf64ba03
Theodore <treeodoreg@gmail.com>
2023-09-10 15:36:42 -0700

    Work on homework

--- homework/codewars/8kyu_even_or_odd.py
+++ homework/codewars/8kyu_even_or_odd.py
@@ -1,12 +1,30 @@
-# Even or odd:
-# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+# # Even or odd:
+# # https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
 
-# Extra homework:
+# # Extra homework:
 
-# 1. Write a function that returns "4" if a number is divisible by 4 and "3" if it's divisible by 3. Return "3 and 4" if it's divisible by both.
-# Call your function with appropriate numbers to test it.
-# def three_four(number):
+# # 1. Write a function that returns "4" if a number is divisible by 4 and "3" if it's divisible by 3. Return "3 and 4" if it's divisible by both.
+# # Call your function with appropriate numbers to test it.
+# # def three_four(number):
+# #     if number%4 == 0 and number%3 == 0:
+# #         return "3 and 4"
+# #     elif number%4 == 0:
+# #         return "4"
+# #     elif number%3 == 0:
+# #         return "3"
+# #     else:
+# #         return "That number doesn't work"
+    
+# # print(three_four(8))
 
-# 2. Write a function that prints 'Small' if a number is less than 10, 'Medium' if a number is between 10 and 100, and 'Large' if a number is greater than 100.
-# Call your function with appropriate numbers to test it.
-# def small_medium_large(number):
+# # 2. Write a function that prints 'Small' if a number is less than 10, 'Medium' if a number is between 10 and 100, and 'Large' if a number is greater than 100.
+# # Call your function with appropriate numbers to test it.
+# def small_medium_large(num):
+#     if num < 10:
+#         print('Small')
+#     elif num >= 10 and num <= 100:
+#         print('Medium')
+#     elif num > 100:
+#         print('Large')
+
+# small_medium_large(100)
--- homework/codewars/8kyu_sum_of_positive.py
+++ homework/codewars/8kyu_sum_of_positive.py
@@ -1,15 +1,42 @@
 # Codewars kata: 8kyu Sum of positive
 # https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+# def positive_sum(arr):
+#     total = 0
+#     for i in range(len(arr)):
+#         if arr[i] > 0:
+#             total += arr[i]
+#     return total
+# print(positive_sum([1,1,-900,6,-20]))
+
+
 
 # Extra homework:
 
 # 1. given an array of number, return the sum of every other number
 #    def sum_every_other(numbers):
+def positive_sum(numbers):
+    total = 0
+    # step: 0, 1, 2, 3, 4, 5
+    # 0, 2, 4, 6, 8, 10, 12
+    for i in range(0, len(numbers), 2):
+        if numbers[i] > 0:
+            total += numbers[i]
+    return total
+# print(positive_sum([6,2,3,5]))
+# # 2. given an array of numbers, return the sum of the first and last number
+def sum_first_and_last(nums):
+    # len(nums) . or subscript by -1
+    return nums[0] + nums[-1]
+# print(sum_first_and_last([]))
 
-# 2. given an array of numbers, return the sum of the first and last number
-#    def sum_first_and_last(numbers):
+
 
 # 3. given an array of numbers, return the sum of every third number
-#    def sum_every_third(numbers):
-
+def sum_every_third(numbers):
+    total = 0
+    for i in range(0, len(numbers), 3):
+        # i is the index. sum a number in the list
+        total += numbers[i]
+    return total
+print(sum_every_third([1,7,9,2,86,44,3]))
 
