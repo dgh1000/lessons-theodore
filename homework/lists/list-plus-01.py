0d13e903b60ad2921a3172ad5d940fcbe5ce7745
Michael Mossey <michaelmossey@gmail.com>
2023-09-23 17:57:30 -0700

    homework added for 9-24-2023 lesson

---
+++ homework/lists/list-plus-01.py
@@ -0,0 +1,44 @@
+# EXAMPLE A:
+
+# my_string1 = "Hi there."
+# my_string2 = "How are you?"
+# combined = my_string1 + my_string2
+# print(combined)
+
+# Question 1:
+# Uncomment example A and run it. First predict what it will do.
+
+# Question 2:
+# Explain in your own words what the code in EXAMPLE A does.
+
+# EXPLANATION
+# The + operator is used to combine strings. It is called
+# "concatenation". The result is a new string that is the
+# two strings combined.
+
+# EXAMPLE B:
+
+# my_list = [10, 3, 13, -15, 20, -215]
+# my_list2 = [12, 1, -5, 6, 8]
+# combined = my_list + my_list2
+# print(combined)
+
+# Uncomment example B and run it. First predict what it will do.
+
+# Question 3:
+# Explain in your own words what the code in EXAMPLE B does.
+
+# EXPLANATION
+# The + operator is can also be used to combine lists. 
+# Note that it has *three* purposes: 
+# 1. to add numbers
+# 2. to combine strings
+# 3. to combine lists
+# Example B shows how to combine two lists. The result is
+# a new list that is the two lists combined.
+
+# Question 4:
+# Make two new lists. Combine them and print the result.
+# Try combining them in the opposite order. What happens?
+
+
---
+++ homework/slices/slices-01.py
@@ -0,0 +1,73 @@
+
+
+my_list = [6, 1, -2, 3, 10, 13]
+
+# Here is a demonstration of indexing or subscripting the list: (please
+# write down the words indexing and subscripting to help you remember them)
+
+# EXAMPLE A:
+
+# Uncomment and run this code. Before you
+# run it, predict what it will do.
+# print(my_list[0])
+
+# Question 1: 
+# Explain in your own words what the code on line 8 does.
+
+# Question 2:
+# Which of the values on line 8 is the "subscript" or "index"?
+
+# Question 3:
+# What "syntax" is used to subscript a list? In other words, 
+# what characters are used and where do they go?
+
+# Question 4:
+# Write a line of code that will print the value 10 from the list.
+# Figure what index you need, and then write the code.
+
+# EXAMPLE B:
+
+# Now we will address the topic of what you need to do to print 
+# several items in a row from the list. You can start like this:
+# Uncomment this code and run it. First predict what it will do.
+
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+
+# Question 5:
+# Modify the code on lines 28 through 30 to print the values
+# 3, 10, and 13 from the list.
+
+# EXAMPLE C:
+
+# Uncomment the following code and run it. First see if
+# you can predict what it will do. You may not remember 
+# it or perhaps we haven't done it before.
+
+# print(my_list[0:3])
+
+# Question 6:
+# Explain in your own words what the code on line 48 does.
+
+# EXPLANATION
+# This syntax is called "slicing" a list. It is a way to
+# get a section of a list. The first number is the starting
+# index and the second number is the ending index. The
+# ending index is not included in the slice. So in this
+# example, the slice starts at index 0 and ends at index 2.
+# The value at index 3 is not included.
+
+# Question 7:
+# What is different between the values printed in EXAMPLE B
+# and the values printed in EXAMPLE C?
+
+# Question 8:
+# Modify the code on line 48 to print the values 3, 10, and 13.
+# What's the last index you need to include? What should the 
+# second number be in the slice?
+
+# EXAMPLE D:
+
+# Uncomment the following code and run it. First see if
+
