99441426cc8e05b2f7e0eac90de00fa333d5359c
Mike Mossey <michaelmossey@gmail.com>
2023-10-11 21:19:16 -0700

    homework for 10-15-23

---
+++ homework/by_date/for-2023-10-15/basics-0490-range-slice-review.py
@@ -0,0 +1,62 @@
+# Topics covered:
+# 1) three forms of range()
+# 2) using range() both to construct a list and to iterate.
+# 3) three forms of slices
+
+# Prerequisites: initial exposure to range, slices, and
+# for loops.
+
+# Question #1:
+# What does the following code print?
+# for i in range(5):
+#     print(i)
+
+# Question #2:
+# What does the following code print?
+# for i in range(2, 12):
+#     print(i)
+
+# Question #3:
+# What does the following code print?
+# for i in range(0, 13, 2):
+#     print(i)
+
+# Question #4:
+# What does the following code print?
+# for i in range(12, -1, -2):
+#     print(i)
+
+# Question #5:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[1:4])
+
+# Question #6:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[:3])
+
+# Question #7:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[3:])
+
+# Question #8:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[0:5:2])
+
+# Question #9:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[::2])
+
+# Question #10:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[4:1:-1])
+
+# Question #11:
+# What does the following code print?
+# my_list = [0, 1, 2, 3, 4]
+# print(my_list[::-1])
---
+++ homework/by_date/for-2023-10-15/basics-0500-range-slice.py
@@ -0,0 +1,52 @@
+# Topics covered: 
+# 1) three forms of range()
+# 2) using range() both to construct a list and to iterate. 
+# 3) three forms of slices
+
+# Prequisites:
+# 1) basic familiarity with lists and for loops.
+
+# Question #1:
+#
+# 1) Write a for loop with range() that prints the numbers 0
+#    through 9.
+# 2) Then print the numbers 2 through 11. 
+# 3) Then print the numbers 0, 2, 4, etc. up to 12. 
+# 4) Then print the numbers 12, 10, 8, etc. down to 0. 
+
+# Question #2:
+#
+# 1) Print the list [0, 1, 2, 3, 4]. Note you are printing a
+#    list, not a number on each line. 
+# 2) Now use range() to construct the same list and print it.
+
+# Question #3:
+#
+my_list = [-1, 7, 42, 0, 9]
+
+# 1) Print the list [7, 42, 0] using a slice of the
+#    form my_list[A:B] where you substitute the correct
+#    values for A and B.
+
+# 2) Print the list [-1, 7, 42] using a slice with
+#    only one number in it (but still has a colon).
+#    Hint: you want to use the form of the slice that
+#    starts at the beginning of the list by default
+
+# 3) Print the list [0, 9] using a slice with
+#    only one number in it (but still has a colon).
+#    Hint: you want to use the form of the slice that
+#    ends the new list at the end of my_list by default.
+
+# 4) Print the list [-1, 42, 9] using a slice with
+#    three numbers in it.
+
+# 5) Print the list [7, 0] using a slice with
+#    three numbers in it.
+
+# 6) Print the list [0, 42, 7] using a slice with
+#    three numbers in it.
+
+# 7) Print the list [9, 0, 42, 7, -1] using a slice with
+#    two colors and a single number.
+
